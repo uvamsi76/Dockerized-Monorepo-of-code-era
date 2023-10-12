0176423e524fc03d2cd9a03831155e07ddacd28c
vamsi <uvamsi76@gmail.com>
2023-10-12 18:20:10 +0530

    edited deploy.sh files

--- apps/backend/deploy.sh
+++ apps/backend/deploy.sh
@@ -2,8 +2,22 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/codes-era_monorepo/Dockerized-Monorepo-of-codesera
+echo moved to Dockerized-Monorepo-of-codesera
+pwd
+echo --------------------------------------------------
 git pull origin main
+echo code pulled
+echo --------------------------------------------------
 mv docker/backend/Dockerfile .
+echo moved docker file
+echo --------------------------------------------------
 sudo docker build . -t backend-docker
+echo docker build
+echo --------------------------------------------------
 sudo docker run -d -p 3010:3010 backend-docker
+echo docker running
+echo --------------------------------------------------
 mv Dockerfile ./docker/backend
+echo move back docker
+echo --------------------------------------------------
+echo --------------------------------------------------
--- apps/frontend-nextapp/deploy.sh
+++ apps/frontend-nextapp/deploy.sh
@@ -2,7 +2,32 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/codes-era_monorepo/Dockerized-Monorepo-of-codesera
+
+echo moved to Dockerized-Monorepo-of-codesera
+pwd
+echo --------------------------------------------------
+
 git pull origin main
+
+echo git pulled
+echo --------------------------------------------------
+
 mv docker/frontend-nextapp/Dockerfile .
+
+echo docker moved
+echo --------------------------------------------------
+
 sudo docker build . -t frontend-nextapp-docker
+
+echo docker built
+echo --------------------------------------------------
+
 sudo docker run -d -p 3000:3000 frontend-nextapp-docker
+
+echo docker ran
+
+mv Dockerfile ./docker/backend
+echo move back docker 
+
+echo --------------------------------------------------
+echo --------------------------------------------------
