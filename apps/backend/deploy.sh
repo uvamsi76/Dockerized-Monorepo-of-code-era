29ee0056a70a80a67b80e8241611906c513ffa88
vamsi <uvamsi76@gmail.com>
2023-10-12 08:28:22 +0530

    changed according to new ec2

--- apps/backend/deploy.sh
+++ apps/backend/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
-cd /home/ubuntu/codes-era_monorepo
+cd /home/ubuntu/codes-era_monorepo/Dockerized-Monorepo-of-codesera
 git pull origin main
 mv docker/backend/Dockerfile .
 sudo docker build . -t backend-docker
--- apps/frontend-nextapp/deploy.sh
+++ apps/frontend-nextapp/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
-cd /home/ubuntu/codes-era_monorepo
+cd /home/ubuntu/codes-era_monorepo/Dockerized-Monorepo-of-codesera
 git pull origin main
 mv docker/frontend-nextapp/Dockerfile .
 sudo docker build . -t frontend-nextapp-docker
-sudo docker run -d -p 3010:3010 frontend-nextapp-docker
+sudo docker run -d -p 3000:3000 frontend-nextapp-docker
